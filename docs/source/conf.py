22ef63aaa9ab290e276919280d29085349c2685f
student1304 <student1304@online.de>
2018-03-16 11:16:16 +0100

    2nd commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,6 +37,7 @@ extensions = ['sphinx.ext.todo',
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['btri_templates']
+html_show_sourcelink = False
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -91,7 +92,8 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'display_version': False}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
